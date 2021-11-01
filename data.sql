1334de47836d315b0efebbd1f117da54b174bfa7
elmar8287 <L-mar@inbox.ru>
2021-11-01 12:19:49 +0400

    Add data into animals table

---
+++ data.sql
@@ -0,0 +1,8 @@
+INSERT INTO animals (animal_name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES ('Agumon','2020.02.03','0','1','10.23');
+INSERT INTO animals (animal_name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES ('Gabumon','2018.10.15','2','1','8');
+INSERT INTO animals (animal_name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES ('Pikachu','2021.01.07','1','0','15.04');
+INSERT INTO animals (animal_name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES ('Devimon','2017.05.12','5','1','11');
