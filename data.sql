95b515ac43ee17ba2dc5ff72c98bf3d03971887a
elmar8287 <L-mar@inbox.ru>
2021-11-01 15:21:18 +0400

    Add data

--- data.sql
+++ data.sql
@@ -6,3 +6,16 @@ INSERT INTO animals (animal_name, date_of_birth, escape_attempts, neutered, weig
 VALUES ('Pikachu','2021.01.07','1','0','15.04');
 INSERT INTO animals (animal_name, date_of_birth, escape_attempts, neutered, weight_kg) 
 VALUES ('Devimon','2017.05.12','5','1','11');
+
+INSERT INTO animals (animal_name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES ('Charmander','2020.02.08','0','0','-11');
+INSERT INTO animals (animal_name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES ('Plantmon','2022.11.15','2','1','-5.7');
+INSERT INTO animals (animal_name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES ('Squirtle','1993.04.02','3','0','-12.13');
+INSERT INTO animals (animal_name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES ('Angemon','2005.06.12','1','1','-45');
+INSERT INTO animals (animal_name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES ('Boarmon','2005.06.07','7','1','20.4');
+INSERT INTO animals (animal_name, date_of_birth, escape_attempts, neutered, weight_kg) 
+VALUES ('Blossom','1998.10.13','3','1','17');
